48aa2b290141b482c45f4d0b6e21dff943ee8e60
Hung <117622033+DucHung59@users.noreply.github.com>
2024-04-05 21:16:46 +0700

    add check js

--- BTL_Web_DictionaryOnline/js/loginForm.js
+++ BTL_Web_DictionaryOnline/js/loginForm.js
@@ -1,15 +1,7 @@
-﻿document.querySelector('#Signup').onclick = () => {
-    if(document.querySelector('.Message').classList.contains('message-login')) {
-        document.querySelector('.Message').classList.remove('message-login');
-    }
-    document.querySelector('.Message').classList.add('message-signup');
-    document.querySelector('#Message-Header').innerHTML = "Sign up for Free!";
-}
+﻿var checkSignup = () => {
 
-document.querySelector('#Login').onclick = () => {
-    if(document.querySelector('.Message').classList.contains('message-signup')) {
-        document.querySelector('.Message').classList.remove('message-signup');
-    }
-    document.querySelector('.Message').classList.add('message-login');
-    document.querySelector('#Message-Header').innerHTML = "Welcome back!";
-}
+};
+
+var checkLogin = () => {
+
+};
