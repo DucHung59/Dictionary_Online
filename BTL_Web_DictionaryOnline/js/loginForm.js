26d5394994a62124b4cd3f1ace2235743d879d7e
Hung <117622033+DucHung59@users.noreply.github.com>
2024-04-06 16:08:27 +0700

    login check

--- BTL_Web_DictionaryOnline/js/loginForm.js
+++ BTL_Web_DictionaryOnline/js/loginForm.js
@@ -1,16 +1,63 @@
-﻿var checkSignup = () => {
+﻿
+function checkLength(input, err, leng) {
+    if (input.length > leng) {
+        err.innerText = `Độ dài kí tự tối đa là: ${leng}`;
+    } else {
+        err.innerText = '';
+    }
+}
 
+
+var checkSignup = () => {
+    let name = document.getElementById('name').value.trim();
+    let user = document.getElementById('user').value.trim();
+    let pass = document.getElementById('pass').value.trim();
+
+    let checkYourName = document.getElementById('checkYourName');
+    let checkUserName = document.getElementById('checkUserName');
+    let checkPass = document.getElementById('checkPass');
+
+    if (name == "") {
+        checkYourName.innerText = 'không được để trống!';
+    } else {
+        checkLength(name, checkYourName, 15);
+    }
+    if (user == "") {
+        checkUserName.innerText = 'không được để trống!';
+    } else {
+        checkLength(user, checkUserName, 8);
+    }
+    if (pass == "") {
+        checkPass.innerText = 'không được để trống!';
+    } else {
+        checkLength(pass, checkPass, 8);
+    }
+    if (checkYourName.innerText.trim() === '' && checkUserName.innerText.trim() === '' &&
+        checkPass.innerText.trim() === '') {
+        return true;
+    }
+    return false;
 };
 
 var checkLogin = () => {
-    let user = document.getElementById('user');
-    let pass = document.getElementById('pass');
-    let checkUserId = document.getElementById('checkUserId');
+    let user = document.getElementById('username').value.trim();
+    let pass = document.getElementById('password').value.trim();
+
+    let checkUserName = document.getElementById('checkUserName');
     let checkPass = document.getElementById('checkPass');
 
-    if (user.value == "") {
-        checkUserId.innerHTML = `<span>Tài khoản không được để trống</span>`;
-    } else if (pass.value == "") {
-        checkPass.innerHTML = `<span>Mật khẩu không được để trống</span>`;
-    } 
+    if (user == "") {
+        checkUserName.innerText = 'không được để trống!';
+    } else {
+        checkUserName.innerText = '';
+    }
+    if (pass == "") {
+        checkPass.innerText = 'không được để trống!';
+    } else {checkUserName
+        checkPass.innerText = '';
+    }
+    if (checkUserName.innerText.trim() === '' && checkPass.innerText.trim() === '') {
+        return true;
+    }
+    return false;
 };
